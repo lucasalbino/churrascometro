e9beba0739d139f522961be33f0fa9d87fc1eae8
Lucas <lucas13.albino@gmail.com>
2022-06-27 18:41:14 -0300

    Criação de estrutura do churrascometro

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// carne - 400g por pessoa | + de 6 horas - 650g
+// cerveja - 1200ml por pessoa | + de 6 horas - 2000 ml
+// refrigerante - 1000ml por pessoa | + 6 horas 1500ml
+
+// crianças valem por 0,5 
+
