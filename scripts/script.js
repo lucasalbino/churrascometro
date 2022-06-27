901ff9ff9be314b13d10b9bf21384c15d1c46c01
Lucas <lucas13.albino@gmail.com>
2022-06-27 19:22:50 -0300

    Estilização e criação de funcionalidade de calculo

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,53 @@
 
 // crianças valem por 0,5 
 
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+
+    let totalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
+    let totalCerveja = carnePP(duracao) * adultos
+    let totalRefrigerante = refrigerantePP(duracao) * adultos + (refrigerantePP(duracao) / 2 * criancas)
+
+    resultado.innerHTML = `<p>${totalCarne / 1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil((totalCerveja / 355))} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${totalRefrigerante / 1000} L de Refrigerante</p>`
+
+    console.log(totalCarne)
+    console.log(totalCerveja)
+    console.log(totalRefrigerante)
+
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200;
+    }
+}
+
+function refrigerantePP(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    } else {
+        return 1000;
+    }
+}
+
